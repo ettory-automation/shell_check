dfe7453696afcb611fed9df1c1ed5336f85b75b5
ettorydevops <ettory.devops@gmail.com>
2025-06-11 14:37:00 -0300

    Update deb_kernel_analyzer.sh

--- src/deb_kernel_analyzer.sh
+++ src/deb_kernel_analyzer.sh
@@ -6,22 +6,22 @@ RED='\033[1;31m'
 MAGENTA='\033[1;35m'
 NC='\033[0m'
 
-get_kernel_ver_installed(){
+get_kernel_ver_installed_deb(){
 	printf "\n${MAGENTA}=== Kernel Versions Installed ===\n${NC}"
 	dpkg --list | grep linux-image | awk '{print $2}' | sort -V | tail -n 3 || true
 }
 
-get_kernel_run(){
+get_kernel_run_deb(){
 	printf "\n${MAGENTA}=== Running Kernel ===\n${NC}"
 	uname -r
 }
 
-get_kernel_update_logs(){
+get_kernel_update_logs_deb(){
 	printf "\n${MAGENTA}=== Kernel Update Logs ===\n${NC}"
 	awk '/Start-Date:|Commandline:|Requested-By:|linux-image/ {print}' /var/log/apt/history.log* 2>/dev/null | tail -n 22 || true
 }
 
-get_status_autoupgrades(){
+get_status_autoupgrades_deb(){
 	printf "\n${MAGENTA}=== Unattended Upgrades Status ===\n${NC}"
 
 	if systemctl list-units --type=service | grep -q "unattended-upgrades.service"; then
@@ -39,10 +39,10 @@ get_status_autoupgrades(){
 
 deb_kernel_check(){
 	clear
-	get_kernel_ver_installed
-	get_kernel_run
-	get_kernel_update_logs
-	get_status_autoupgrades
+	get_kernel_ver_installed_deb
+	get_kernel_run_deb
+	get_kernel_update_logs_deb
+	get_status_autoupgrades_deb
 
 	printf "\n${MAGENTA}Pressione ENTER para retornar ao menu...${NC}"
 	read -r
