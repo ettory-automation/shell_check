b74f702b96d78d27d905e4f9f83345060e6ad400
ettorydevops <ettory.devops@gmail.com>
2025-06-10 21:30:14 -0300

    Update deb_kernel_analyzer.sh

--- src/deb_kernel_analyzer.sh
+++ src/deb_kernel_analyzer.sh
@@ -8,22 +8,22 @@ NC='\033[0m'
 clear
 
 function get_kernel_ver_installed(){
-	echo "=== Kernel Versions Installed ==="
+	printf "\n${MAGENTA}=== Kernel Versions Installed ===\n${NC}"
 	dpkg --list | grep linux-image | awk '{print $2}' | sort -V | tail -n 3 || true
 }
 
 function get_kernel_run(){
-	echo -e "\n=== Running Kernel ==="
+	printf "\n${MAGENTA}=== Running Kernel ===\n${NC}"
 	uname -r
 }
 
 function get_kernel_update_logs(){
-	echo -e "\n=== Kernel Update Logs ==="
+	printf "\n${MAGENTA}=== Kernel Update Logs ===\n${NC}"
 	awk '/Start-Date:|Commandline:|Requested-By:|linux-image/ {print}' /var/log/apt/history.log* 2>/dev/null | tail -n 22 || true
 }
 
 function get_status_autoupgrades(){
-	echo -e "\n=== Unattended Upgrades Status ==="
+	printf "\n${MAGENTA}=== Unattended Upgrades Status ===\n${NC}"
 
 	if systemctl list-units --type=service | grep -q "unattended-upgrades.service"; then
     		if systemctl is-active --quiet unattended-upgrades.service; then
